0fafafe0b2b9d66b2d3463c9f9d6e99323ed3ae2
adilenecorrea <adilenecorrea05@gmail.com>
2024-10-21 09:31:34 -0600

    Add files via upload

---
+++ P00Presentacion.php
@@ -0,0 +1,17 @@
+<?php
+/* CBTIS 89
+   Programa que presente mis datos   
+   Brenda Adilene Correa Antuna
+   3°A Programacion Matutino 
+*/
+echo  "** MIS DATOS **",  "<br>" ,  "<br>";
+
+echo "Brenda Adilene Correa Antuna";
+echo "<br>";
+echo "Mi escuela es CBTIS 89";
+echo "<br>";
+echo "Estoy en la carrera de programación en el tercer semestre";
+echo "<br>";
+echo "Tengo 16 años";
+echo "<br>";
+echo "Vivo en Durango";
