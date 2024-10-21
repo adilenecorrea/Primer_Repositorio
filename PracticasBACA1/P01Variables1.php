ac5e4b4ad4c0a3cd90aa8a992e7b5592c79a043a
adilenecorrea <adilenecorrea05@gmail.com>
2024-10-21 09:09:37 -0600

    Add files via upload

---
+++ PracticasBACA1/P01Variables1.php
@@ -0,0 +1,24 @@
+<?php
+/* CBTIS 89
+   Progrma que indique el sueldo de Juan  
+   Brenda Adilene Correa Antuna
+   3°A Programacion Matutino 
+*/
+$día = 24; //Se declara una variable de tipo interger.
+$sueldo = 758.43; //Se declara una variable de tipo double.
+$nombre = "Juan"; //Se declara una variable de tipo string.
+$exite = true;  //Se declara una variable de tipo boolean.
+
+echo "Variable entera:";
+echo $día;echo "<br>";
+echo "Variable double:";
+echo $sueldo;echo "<br>";
+echo "Variable string:";
+echo $nombre; echo "<br>";
+echo "Variable boolean:";
+echo $exite;
+echo "<br>";
+$cadena1= "Hola";
+$cadena2= "Mundo";
+echo $cadena1. " ".$cadena2;
+?>
